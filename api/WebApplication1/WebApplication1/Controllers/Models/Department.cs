d1f4c5cee8ae7dce06336d617d5434036789071b
Silviu <Silviu@DESKTOP-DECL27M>
2022-07-29 17:03:16 +0300

    Add project files.

---
+++ api/WebApplication1/WebApplication1/Controllers/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+
+
+        public string DepartmentName { get; set; }
+
+    }
+}
---
+++ api/WebApplication1/WebApplication1/Controllers/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+
+        public string EmployeeName { get; set; }
+
+
+        public string Department { get; set; }
+
+
+        public string DateOfJoining { get; set; }
+
+
+        public string PhotoFileName { get; set; }
+    }
+}
